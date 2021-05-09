5eb034c26845f463e8e5cb0f42e513a9511c8974
Leo-Chan01 <ephraimleo16@gmail.com>
2021-05-09 21:03:31 +0100

    Initial Commit Lecture Material
    Signed-off-by: Leo-Chan01 <ephraimleo16@gmail.com>

---
+++ app/src/main/java/com/raym/dev101/Layouts.java
@@ -0,0 +1,16 @@
+package com.raym.dev101;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.raym.dev002.R;
+
+public class Layouts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layouts);
+    }
+}
